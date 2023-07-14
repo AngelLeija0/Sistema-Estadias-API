d34cfd876c8b9e7bbea1efa880586dfd03c7de3b
Angel Leija <angelgaelleija2@gmail.com>
2023-07-14 08:02:30 -0500

    Generar excel de alumnos asesorados agregado

--- routes/lado-asesor.js
+++ routes/lado-asesor.js
@@ -4,6 +4,8 @@ const { ObjectId } = mongoose.Types;
 const express = require('express');
 const router = express.Router();
 
+const ExcelJS = require('exceljs');
+
 const Estadia = require('../models/estadia');
 const Asesor = require('../models/asesor');
 const Alumno = require('../models/alumno');
@@ -65,6 +67,91 @@ router.post('/inicio', async (req, res) => {
             }
         }
         res.json(alumnos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// POST - Generar Excel de alumnos asesorados
+router.post('/inicio/excel', async (req, res) => {
+    try {
+        let alumnos = [];
+        const filtro = req.body.filtro;
+        const estadias = await Estadia.find({
+            idAsesor: new ObjectId(req.body.idAsesor)
+        });
+        for (const estadia of estadias) {
+            const idAlumno = estadia._doc.idAlumno;
+            const busqueda = {
+                _id: new ObjectId(idAlumno)
+            };
+            if (filtro.buscador) {
+                const textoBusqueda = filtro.buscador;
+                const regex = new RegExp(textoBusqueda, 'i');
+                busqueda.$or = [
+                    { 'datosPersonales.nombres.nombre': regex },
+                    { 'datosPersonales.nombres.apPaterno': regex },
+                    { 'datosPersonales.nombres.apMaterno': regex },
+                    { 'datosPersonales.privado.matricula': regex },
+                ];
+                const numeroPartes = textoBusqueda.split(" ");
+                if (numeroPartes.length >= 2) {
+                    const nombre = numeroPartes.slice(0, numeroPartes.length - 2).join(" ");
+                    const apPaterno = numeroPartes[numeroPartes.length - 2];
+                    const apMaterno = numeroPartes[numeroPartes.length - 1];
+                    busqueda.$or.push({ 'datosPersonales.nombres.nombre': nombre });
+                    busqueda.$or.push({ 'datosPersonales.nombres.apPaterno': apPaterno });
+                    busqueda.$or.push({ 'datosPersonales.nombres.apMaterno': apMaterno });
+                }
+            }
+            if (filtro.nivelAcademico) {
+                busqueda["datosAcademicos.nivelAcademico"] = filtro.nivelAcademico;
+            }
+            if (filtro.carrera) {
+                busqueda["datosAcademicos.carrera"] = filtro.carrera;
+            }
+            if (filtro.area) {
+                busqueda["datosAcademicos.area"] = filtro.area;
+            }
+            const alumno = await Alumno.findOne(busqueda);
+            if (alumno) {
+                const infoAlumno = {
+                    idAlumno: idAlumno,
+                    nombre: alumno.datosPersonales.nombres.nombre,
+                    apPaterno: alumno.datosPersonales.nombres.apPaterno,
+                    apMaterno: alumno.datosPersonales.nombres.apMaterno,
+                    matricula: alumno.datosPersonales.privado.matricula,
+                    nivelAcademico: alumno.datosAcademicos.nivelAcademico,
+                    carrera: alumno.datosAcademicos.carrera,
+                    area: alumno.datosAcademicos.area
+                };
+                alumnos.push(infoAlumno);
+            }
+        }
+        const data = alumnos;
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Datos');
+
+        worksheet.columns = [
+            { header: 'Nombre', key: 'nombre' },
+            { header: 'Apellido paterno', key: 'apPaterno' },
+            { header: 'Apellido materno', key: 'apMaterno' },
+            { header: 'Matricula', key: 'matricula' },
+            { header: 'Nivel academico', key: 'nivelAcademico' },
+            { header: 'Carrera', key: 'carrera' },
+            { header: 'Area', key: 'area' },
+        ];
+
+        data.forEach((row) => {
+            worksheet.addRow(row);
+        });
+
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', 'attachment; filename=alumnos-en-proceso.xlsx');
+
+        return workbook.xlsx.write(res).then(() => {
+            res.status(200).end();
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
